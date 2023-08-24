a53461cde3bbaf3e30e365845fe715de7223a5b7
rafi <m.rafi.ardhani@gmail.com>
2023-08-24 15:04:12 +0700

    pembuatan api crud

--- lib/mb_locationinq.go
+++ lib/mb_locationinq.go
@@ -4,12 +4,14 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"log"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func MbLocInq(c *gin.Context) {
@@ -26,31 +28,28 @@ func MbLocInq(c *gin.Context) {
 	var biz_hour string
 	var latitude string
 	var longitude string
-	var ReqStruct MbLocationReq
+	// var ReqStruct MbLocationReq
 	var ResultElements []MbLocationRespResult
 
 	// if err := c.ShouldBindJSON(&ReqStruct); err != nil {
 	// 	c.JSON(400, gin.H{"Error ": err.Error()})
 	// 	return
 	// }
-	reqJSON, errdec := AESDecryptRequestJSON(c)
-	if errdec != "" {
-		return
-	}
-	json.Unmarshal(reqJSON, &ReqStruct)
-	if err := c.ShouldBindBodyWith(&ReqStruct, binding.JSON); err != nil {
-		MotionRes := MotionJSON{
-			Error: err.Error(),
-		}
+	// reqJSON, errdec := AESDecryptRequestJSON(c)
+	// if errdec != "" {
+	// 	return
+	// }
+	// json.Unmarshal(reqJSON, &ReqStruct)
 
-		jsonRes, _ := json.Marshal(MotionRes)
-		encryptRes := AESEncrypt(string(jsonRes))
-		c.JSON(400, gin.H{"Content": encryptRes})
-		return
-	}
-	db, err := sql.Open("mysql", "varuser:mnc123@tcp("+dbaddr+":"+dbport+")/mncmbank")
+	// if err := c.ShouldBindBodyWith(&ReqStruct, binding.JSON); err != nil {
+	// 	log.Print(err.Error())
+	// 	c.JSON(400, gin.H{"Error": err.Error()})
+	// 	return
+	// }
+
+	db, err := sql.Open("mysql", "root:@tcp("+"localhost"+":"+"3306"+")/mnc")
 	if err != nil {
-		log.Println(err.Error())
+		fmt.Print(err.Error())
 	}
 	defer db.Close()
 	queryget, err := db.Query(`SELECT
@@ -88,7 +87,7 @@ func MbLocInq(c *gin.Context) {
 				mb_bank_location
 			`)
 	if err != nil {
-		log.Println(err.Error())
+		c.AbortWithError(500, err)
 	}
 	for queryget.Next() {
 		queryget.Scan(&utype, &name, &address, &city, &postcode, &state, &country, &phone, &fax, &email, &biz_hour, &latitude, &longitude)
@@ -112,13 +111,392 @@ func MbLocInq(c *gin.Context) {
 	fmt.Println(ResultElements)
 	RespVer := MbLocationResp{
 		Success:     true,
-		Message:     GetErrorDesc("M1", "Mobe"),
+		Message:     "SUKSES BRO",
 		MessageCode: "M1",
 		Results:     ResultElements,
 	}
-	Respjson, _ := json.Marshal(RespVer)
+	// Respjson, _ := json.Marshal(RespVer)
+
+	c.JSON(200, RespVer)
 	// c.String(200, string(Respjson))
-	c.JSON(200, gin.H{
-		"Content": AESEncrypt(string(Respjson)),
-	})
+	// c.JSON(200, gin.H{
+	// 	"Content": AESEncrypt(string(Respjson)),
+	// })
+}
+
+// Inside the lib package
+
+func MbDeleteATM(c *gin.Context) {
+	//mengambil nilai dari parameter "id" dari URL dengan menggunakan c.Param("id")
+	ID := c.Param("id")
+	if ID == "" {
+		c.JSON(400, gin.H{"error": "Invalid request data: ID parameter is required"})
+		return
+	}
+	//konversi menjadi integer menggunakan strconv.Atoi(ID)
+	idInt, err := strconv.Atoi(ID)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request data: ID parameter must be an integer"})
+		return
+	}
+	/*
+		koneksi ke database Parameter pertama adalah tipe database yang digunakan ("mysql"),
+		diikuti oleh informasi koneksi seperti username, password, host, dan port.
+		Jika terjadi error dalam pembukaan koneksi,
+		fungsi akan memberikan respons HTTP dengan status 500 (Internal Server Error) dan mengembalikan error yang terjadi
+	*/
+	db, err := sql.Open("mysql", "root:@tcp("+"localhost"+":"+"3306"+")/mnc")
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	//defer db.Close(). Hal ini memastikan bahwa koneksi database akan ditutup
+	defer db.Close()
+
+	_, err = db.Exec("DELETE FROM mb_atm_location WHERE id = ?", idInt)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	/*
+		Jika eksekusi query berhasil, fungsi akan memberikan respons HTTP dengan status 200 (OK)
+		dan mengembalikan objek JSON yang berisi pesan sukses bahwa data ATM berhasil dihapus.
+	*/
+	c.JSON(200, gin.H{"message": "ATM data deleted successfully"})
+}
+
+func MbDeleteBank(c *gin.Context) {
+	ID := c.Param("id")
+	if ID == "" {
+		c.JSON(400, gin.H{"error": "Invalid request data: ID parameter is required"})
+		return
+	}
+
+	idInt, err := strconv.Atoi(ID)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request data: ID parameter must be an integer"})
+		return
+	}
+
+	db, err := gorm.Open(mysql.Open("root:@tcp("+"localhost"+":"+"3306"+")/mnc"), &gorm.Config{})
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	var mbBankLocation mb_bank_location
+	result := db.Where("id = ?", idInt).Delete(&mbBankLocation)
+	if result.Error != nil {
+		c.AbortWithError(500, result.Error)
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "Bank data deleted successfully"})
+}
+
+func MbBankAdd(c *gin.Context) {
+	var req MbLocationReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	dsn := "root:@tcp(" + "localhost" + ":" + "3306" + ")/mnc"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	mbBankLocations := mb_bank_location{
+		Utype:     req.Utype,
+		Name:      req.Name,
+		Address:   req.Addr,
+		City:      req.City,
+		Postcode:  req.Postcode,
+		State:     req.State,
+		Country:   req.Country,
+		Phone:     req.Phone,
+		Fax:       req.Fax,
+		Email:     req.Email,
+		Bizhour:   req.Bizhour,
+		Latitude:  req.Latitude,
+		Longitude: req.Longitude,
+		CreatedAt: time.Now(),
+	}
+
+	var request map[string]interface{}
+	temp, _ := json.Marshal(mbBankLocations)
+	json.Unmarshal(temp, &request)
+
+	result := db.Debug().Table("mb_bank_location").Create(&request)
+	if result.Error != nil {
+		c.AbortWithError(500, result.Error)
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "Bank data added successfully"})
+}
+
+func MbAtmAdd(c *gin.Context) {
+	var req MbLocationReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	currentTime := time.Now().Format(time.RFC3339)
+
+	db, err := sql.Open("mysql", "root:@tcp("+"localhost"+":"+"3306"+")/mnc")
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+		INSERT INTO mb_atm_location (
+			utype, NAME, address, city, postcode, state, country,
+			phone, fax, email, biz_hour, latitude, longitude,
+			created, modified
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`, req.Utype, req.Name, req.Addr, req.City, req.Postcode, req.State, req.Country,
+		req.Phone, req.Fax, req.Email, req.Bizhour, req.Latitude, req.Longitude, currentTime, nil)
+
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "ATM data added successfully"})
+}
+
+// Fungsi untuk mengupdate data bank
+func MbBankUpdate(c *gin.Context) {
+	ID := c.Param("id")
+	if ID == "" {
+		c.JSON(400, gin.H{"error": "Invalid request data"})
+		return
+	}
+
+	idInt, err := strconv.Atoi(ID)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request data: ID parameter must be an integer"})
+		return
+	}
+
+	var req MbLocationReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	currentTime := time.Now().Format(time.RFC3339)
+
+	db, err := sql.Open("mysql", "root:@tcp("+"localhost"+":"+"3306"+")/mnc")
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	defer db.Close()
+
+	updateQuery := "UPDATE mb_bank_location SET"
+	values := []interface{}{}
+
+	updateQuery += " modified = ?,"
+	values = append(values, currentTime)
+
+	if req.Name != "" {
+		updateQuery += " NAME = ?,"
+		values = append(values, req.Name)
+	}
+
+	if req.Email != "" {
+		updateQuery += " email = ?,"
+		values = append(values, req.Email)
+	}
+
+	if req.Utype != "" {
+		updateQuery += " utype = ?,"
+		values = append(values, req.Utype)
+	}
+
+	if req.Addr != "" {
+		updateQuery += " address = ?,"
+		values = append(values, req.Addr)
+	}
+
+	if req.City != "" {
+		updateQuery += " city = ?,"
+		values = append(values, req.City)
+	}
+
+	if req.Postcode != "" {
+		updateQuery += " postcode = ?,"
+		values = append(values, req.Postcode)
+	}
+
+	if req.State != "" {
+		updateQuery += " state = ?,"
+		values = append(values, req.State)
+	}
+
+	if req.Country != "" {
+		updateQuery += " country = ?,"
+		values = append(values, req.Country)
+	}
+
+	if req.Phone != "" {
+		updateQuery += " phone = ?,"
+		values = append(values, req.Phone)
+	}
+
+	if req.Fax != "" {
+		updateQuery += " fax = ?,"
+		values = append(values, req.Fax)
+	}
+
+	if req.Bizhour != "" {
+		updateQuery += " biz_hour = ?,"
+		values = append(values, req.Bizhour)
+	}
+
+	if req.Latitude != "" {
+		updateQuery += " latitude = ?,"
+		values = append(values, req.Latitude)
+	}
+
+	if req.Longitude != "" {
+		updateQuery += " longitude = ?,"
+		values = append(values, req.Longitude)
+	}
+
+	updateQuery = strings.TrimSuffix(updateQuery, ",")
+
+	updateQuery += " WHERE id = ?"
+	values = append(values, idInt)
+
+	_, err = db.Exec(updateQuery, values...)
+
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	c.JSON(200, gin.H{"message": "Bank data updated successfully"})
+}
+
+// Fungsi untuk mengupdate data ATM
+func MbAtmUpdate(c *gin.Context) {
+	ID := c.Param("id")
+	if ID == "" {
+		c.JSON(400, gin.H{"error": "Invalid request data"})
+		return
+	}
+
+	idInt, err := strconv.Atoi(ID)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request data: ID parameter must be an integer"})
+		return
+	}
+
+	var req MbLocationReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	currentTime := time.Now().Format(time.RFC3339)
+
+	db, err := sql.Open("mysql", "root:@tcp("+"localhost"+":"+"3306"+")/mnc")
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	defer db.Close()
+
+	updateQuery := "UPDATE mb_atm_location SET"
+	values := []interface{}{}
+
+	updateQuery += " modified = ?,"
+	values = append(values, currentTime)
+
+	if req.Name != "" {
+		updateQuery += " NAME = ?,"
+		values = append(values, req.Name)
+	}
+
+	if req.Email != "" {
+		updateQuery += " email = ?,"
+		values = append(values, req.Email)
+	}
+
+	if req.Utype != "" {
+		updateQuery += " utype = ?,"
+		values = append(values, req.Utype)
+	}
+
+	if req.Addr != "" {
+		updateQuery += " address = ?,"
+		values = append(values, req.Addr)
+	}
+
+	if req.City != "" {
+		updateQuery += " city = ?,"
+		values = append(values, req.City)
+	}
+
+	if req.Postcode != "" {
+		updateQuery += " postcode = ?,"
+		values = append(values, req.Postcode)
+	}
+
+	if req.State != "" {
+		updateQuery += " state = ?,"
+		values = append(values, req.State)
+	}
+
+	if req.Country != "" {
+		updateQuery += " country = ?,"
+		values = append(values, req.Country)
+	}
+
+	if req.Phone != "" {
+		updateQuery += " phone = ?,"
+		values = append(values, req.Phone)
+	}
+
+	if req.Fax != "" {
+		updateQuery += " fax = ?,"
+		values = append(values, req.Fax)
+	}
+
+	if req.Bizhour != "" {
+		updateQuery += " biz_hour = ?,"
+		values = append(values, req.Bizhour)
+	}
+
+	if req.Latitude != "" {
+		updateQuery += " latitude = ?,"
+		values = append(values, req.Latitude)
+	}
+
+	if req.Longitude != "" {
+		updateQuery += " longitude = ?,"
+		values = append(values, req.Longitude)
+	}
+
+	// Menghilangkan koma terakhir dari query
+	updateQuery = strings.TrimSuffix(updateQuery, ",")
+
+	updateQuery += " WHERE id = ?"
+	values = append(values, idInt)
+
+	_, err = db.Exec(updateQuery, values...)
+
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "ATM data updated successfully"})
 }
--- lib/struct.go
+++ lib/struct.go
@@ -1,31 +1,63 @@
 package lib
 
 type MbLocationReq struct {
-	Uuid				string	`json:"6d24" binding:"required"`
-	Gcn					string	`json:"c318" bindind:"required"`
-	// Reqtype				string	`json:"reqtype" binding:"required"`
+	//Uuid string `json:"6d24" `
+	//Gcn  string `json:"c318" `
+	// Reqtype		string`json:"reqtype" binding:"required"`
+	Utype     string `json:"utype"`
+	Created   string `json:"created,omitempty"`
+	Modified  string `json:"modified,omitempty"`
+	Name      string `json:"name" binding:"required,min=5,max=20"`
+	Addr      string `json:"address"`
+	City      string `json:"city"`
+	Postcode  string `json:"postcode,omitempty"`
+	State     string `json:"state,omitempty"`
+	Country   string `json:"country,omitempty"`
+	Phone     string `json:"phone,omitempty"`
+	Fax       string `json:"fax,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Bizhour   string `json:"bizhour,omitempty"`
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
 }
 
 type MbLocationResp struct {
-	MessageCode			string	`json:"messagecode"`
-	Message				string	`json:"message"`
-	Success				bool	`json:"success"`
-	Results				[]MbLocationRespResult	`json:"results"`
+	MessageCode string                 `json:"messagecode"`
+	Message     string                 `json:"message"`
+	Success     bool                   `json:"success"`
+	Results     []MbLocationRespResult `json:"results"`
 }
 
 type MbLocationRespResult struct {
-	Unittype		string	`json:"utype"`
-	Name			string	`json:"name"`
-	Addr			string	`json:"address"`
-	City			string	`json:"city"`
-	Postcode		string	`json:"postcode,omitempty"`
-	State			string	`json:"state,omitempty"`
-	Country			string	`json:"country,omitempty"`
-	Phone			string	`json:"phone,omitempty"`
-	Fax				string	`json:"fax,omitempty"`
-	Email			string	`json:"email,omitempty"`
-	Bizhour			string	`json:"bizhour,omitempty"`
-	Latitude		string	`json:"latitude"`
-	Longitude		string	`json:"longitude"`
+	Unittype  string `json:"utype"`
+	Name      string `json:"name"`
+	Addr      string `json:"address"`
+	City      string `json:"city"`
+	Postcode  string `json:"postcode,omitempty"`
+	State     string `json:"state,omitempty"`
+	Country   string `json:"country,omitempty"`
+	Phone     string `json:"phone,omitempty"`
+	Fax       string `json:"fax,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Bizhour   string `json:"bizhour,omitempty"`
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
 }
 
+type mb_bank_location struct {
+	ID        uint        `gorm:"primaryKey"`
+	Utype     string      `json:"utype"`
+	Name      string      `json:"name"`
+	Address   string      `json:"address"`
+	City      string      `json:"city"`
+	Postcode  string      `json:"postcode,omitempty"`
+	State     string      `json:"state,omitempty"`
+	Country   string      `json:"country,omitempty"`
+	Phone     string      `json:"phone,omitempty"`
+	Fax       string      `json:"fax,omitempty"`
+	Email     string      `json:"email,omitempty"`
+	Bizhour   string      `json:"biz_hour,omitempty"`
+	Latitude  string      `json:"latitude"`
+	Longitude string      `json:"longitude"`
+	CreatedAt interface{} `json:"created"`
+}
